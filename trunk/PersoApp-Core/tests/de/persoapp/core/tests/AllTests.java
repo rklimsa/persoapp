0ed5e6002a9d82db40df1500148f873979aa9470
rico.klimsa@ageto.net <rico.klimsa@ageto.net@1296ccec-e324-93b5-48e2-b0d15f74e564>
2015-04-16 08:32:03 +0000

    TestSuite to execute all tests with one click

--- trunk/PersoApp-Core/tests/de/persoapp/core/tests/AllTests.java
+++ trunk/PersoApp-Core/tests/de/persoapp/core/tests/AllTests.java
@@ -52,14 +52,56 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.persoapp.core.tests.core.ECardWorkerTest;
+import de.persoapp.core.tests.core.card.CMacTest;
+import de.persoapp.core.tests.core.card.CardHandlerTest;
+import de.persoapp.core.tests.core.card.EC_GlobalsTest;
+import de.persoapp.core.tests.core.card.ISOSMTransportTest;
+import de.persoapp.core.tests.core.card.JSCIOTransportTest;
+import de.persoapp.core.tests.core.card.PACETest;
+import de.persoapp.core.tests.core.client.EAC_InfoTest;
+import de.persoapp.core.tests.core.client.ECardSessionTest;
+import de.persoapp.core.tests.core.client.IMainViewTest;
+import de.persoapp.core.tests.core.client.MainViewEventListenerTest;
+import de.persoapp.core.tests.core.client.PropertyResolverTest;
+import de.persoapp.core.tests.core.client.SecureHolderTest;
+import de.persoapp.core.tests.core.paos.MiniHttpClientTest;
+import de.persoapp.core.tests.core.paos.PAOSInitiatorFactoryTest;
+import de.persoapp.core.tests.core.paos.PAOSInitiatorTest;
+import de.persoapp.core.tests.core.tls.BCTlsAuthenticationTest;
+import de.persoapp.core.tests.core.tls.BCTlsSessionTest;
+import de.persoapp.core.tests.core.tls.BCTlsSocketFactoryImplTest;
+import de.persoapp.core.tests.core.tls.BCTlsSocketImplTest;
+import de.persoapp.core.tests.core.tls.TLSClientTest;
+import de.persoapp.core.tests.core.tls.TLSPSKClientTest;
+import de.persoapp.core.tests.core.util.ArrayToolTest;
+import de.persoapp.core.tests.core.util.HexTest;
+import de.persoapp.core.tests.core.util.MyHexBinaryAdapterTest;
+import de.persoapp.core.tests.core.util.TLVTest;
+import de.persoapp.core.tests.core.util.UtilTest;
+import de.persoapp.core.tests.core.ws.WebServiceTest;
+import de.persoapp.core.tests.core.ws.engine.WSContainerTest;
+import de.persoapp.core.tests.core.ws.engine.WSEndpointTest;
+
 /**
- * Class for all Testcases.
+ * Class for all testcases.
  * 
  * @author Rico Klimsa
  */
 @RunWith(Suite.class)
 @SuiteClasses({ Complete.class, WebServiceTest.class, ECardWorkerTest.class,
-		ArrayToolTest.class })
+		ArrayToolTest.class, MyHexBinaryAdapterTest.class, UtilTest.class,
+		HexTest.class, CardHandlerTest.class, CMacTest.class,
+		EC_GlobalsTest.class, ISOSMTransportTest.class,
+		JSCIOTransportTest.class, PACETest.class, EAC_InfoTest.class,
+		ECardSessionTest.class, IMainViewTest.class,
+		MainViewEventListenerTest.class, PropertyResolverTest.class,
+		SecureHolderTest.class, MiniHttpClientTest.class,
+		PAOSInitiatorTest.class, PAOSInitiatorFactoryTest.class,
+		BCTlsAuthenticationTest.class, BCTlsSessionTest.class,
+		BCTlsSocketFactoryImplTest.class, BCTlsSocketImplTest.class,
+		TLSClientTest.class, TLSPSKClientTest.class, WSContainerTest.class,
+		WSEndpointTest.class,TLVTest.class })
 public class AllTests {
 
 }
