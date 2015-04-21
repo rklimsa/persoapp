b8d546b89b5cf06d9953d3da5604080932f8dbf0
christian.kahlo@ageto.net <christian.kahlo@ageto.net@1296ccec-e324-93b5-48e2-b0d15f74e564>
2015-04-21 16:29:49 +0000

    added fix for tcTokenURLs accidently decoding "+" into whitespace, replace all whitespaces with "+" afterwards

--- trunk/PersoApp-Core/src/de/persoapp/core/ECApiHttpHandler.java
+++ trunk/PersoApp-Core/src/de/persoapp/core/ECApiHttpHandler.java
@@ -1,49 +1,49 @@
 /**
- *
+ * 
  * COPYRIGHT (C) 2010, 2011, 2012, 2013, 2014 AGETO Innovation GmbH
- *
+ * 
  * Authors Christian Kahlo, Ralf Wondratschek
- *
+ * 
  * All Rights Reserved.
- *
+ * 
  * Contact: PersoApp, http://www.persoapp.de
- *
+ * 
  * @version 1.0, 30.07.2013 13:50:47
- *
+ * 
  *          This file is part of PersoApp.
- *
+ * 
  *          PersoApp is free software: you can redistribute it and/or modify it
  *          under the terms of the GNU Lesser General Public License as
  *          published by the Free Software Foundation, either version 3 of the
  *          License, or (at your option) any later version.
- *
+ * 
  *          PersoApp is distributed in the hope that it will be useful, but
  *          WITHOUT ANY WARRANTY; without even the implied warranty of
  *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *          Lesser General Public License for more details.
- *
+ * 
  *          You should have received a copy of the GNU Lesser General Public
  *          License along with PersoApp. If not, see
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  *          Diese Datei ist Teil von PersoApp.
- *
+ * 
  *          PersoApp ist Freie Software: Sie können es unter den Bedingungen der
  *          GNU Lesser General Public License, wie von der Free Software
  *          Foundation, Version 3 der Lizenz oder (nach Ihrer Option) jeder
  *          späteren veröffentlichten Version, weiterverbreiten und/oder
  *          modifizieren.
- *
+ * 
  *          PersoApp wird in der Hoffnung, dass es nützlich sein wird, aber OHNE
  *          JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
  *          Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN
  *          ZWECK. Siehe die GNU Lesser General Public License für weitere
  *          Details.
- *
+ * 
  *          Sie sollten eine Kopie der GNU Lesser General Public License
  *          zusammen mit diesem Programm erhalten haben. Wenn nicht, siehe
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 package de.persoapp.core;
 
@@ -151,8 +151,10 @@ public final class ECApiHttpHandler implements HttpHandler {
 
 		System.out.println("qs: " + qs);
 
-		final URL tcTokenURL = qs != null && qs.contains(TC_TOKEN_URL) ? new URL(URLDecoder.decode(qs.substring(qs
-				.indexOf(TC_TOKEN_URL) + TC_TOKEN_URL.length()))) : null;
+		// decode URL but replace resulting whitespace characters with "+", this happens in session IDs
+		// from Governikus
+		final URL tcTokenURL = qs != null && qs.contains(TC_TOKEN_URL) ? new URL(URLDecoder.decode(
+				qs.substring(qs.indexOf(TC_TOKEN_URL) + TC_TOKEN_URL.length())).replace(' ', '+')) : null;
 
 		final String referer = he.getRequestHeaders().getFirst("Referer");
 
@@ -182,6 +184,7 @@ public final class ECApiHttpHandler implements HttpHandler {
 						} else {
 							sendResponse(he, 400, null, result);
 						}
+						return;
 					} else {
 						System.out.println("Warning to result!");
 						throw new FileNotFoundException("missing result");
